315d545f15289202dc46eef751a8752150670bfc
Giovana Z Corsetti <giovana.corsetti@outlook.com>
2024-11-03 12:26:10 -0300

    receitas ok

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
 function ajustar_altura() {
-    var iframe = document.getElementById('mainFrame');
+    let iframe = document.getElementById('mainFrame');
+    try {
+        iframe.style.height = iframe.contentWindow.document.documentElement.scrollHeight + 'px';
+    } catch (error) {
+        console.error('Erro ao ajustar altura do iframe:', error);
+    }
+}
+
+function ajustar_altura2() {
+    let iframe = document.getElementById('perfilFrame');
     try {
         iframe.style.height = iframe.contentWindow.document.documentElement.scrollHeight + 'px';
     } catch (error) {
@@ -9,5 +18,7 @@ function ajustar_altura() {
 
 // Atualiza a altura periodicamente
 setInterval(ajustar_altura, 500);
+setInterval(ajustar_altura2, 500);
 
 window.addEventListener('resize', ajustar_altura);
+window.addEventListener('resize', ajustar_altura2);
